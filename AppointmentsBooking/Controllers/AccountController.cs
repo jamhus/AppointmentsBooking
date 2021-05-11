5269512b4ea0b06e7a3418f497053356d8e333ee
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-12 01:20:43 +0200

    created controller

---
+++ AppointmentsBooking/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using AppointmentsBooking.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentsBooking.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
