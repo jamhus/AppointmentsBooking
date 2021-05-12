af0becaf8a49af7e676dd2a5708a421c0d00c8f6
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-12 12:15:26 +0200

    register service identity for injection

--- AppointmentsBooking/Startup.cs
+++ AppointmentsBooking/Startup.cs
@@ -2,6 +2,7 @@ using AppointmentsBooking.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,8 +30,8 @@ namespace AppointmentsBooking
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
-
             services.AddControllersWithViews();
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<DataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
