92a12666d590d4115d234afc05f64447536d19e0
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-12 13:45:03 +0200

    edited registration model

--- AppointmentsBooking/Models/ViewModels/RegisterViewModel.cs
+++ AppointmentsBooking/Models/ViewModels/RegisterViewModel.cs
@@ -17,16 +17,15 @@ namespace AppointmentsBooking.Models.ViewModels
 
         [Required]
         [DataType(DataType.Password)]
-        [StringLength(100,ErrorMessage = "The {0} must be at least {2} characters long.",MinimumLength =6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name ="Confirm Password")]
-        [Compare("Password", ErrorMessage = "The password and the confirm password does not match")]
-        public int ConfirmPassword { get; set; }
-
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
         [Required]
-        [Display(Name ="Role Name")]
+        [Display(Name = "Role Name")]
         public string RoleName { get; set; }
     }
 }
