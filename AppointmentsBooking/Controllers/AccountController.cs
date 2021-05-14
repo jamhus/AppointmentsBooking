74675c53e10ff46e222935b3e598d2db2746d76c
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-14 15:07:20 +0200

    Display doctor list for admin

--- AppointmentsBooking/Controllers/AccountController.cs
+++ AppointmentsBooking/Controllers/AccountController.cs
@@ -40,7 +40,7 @@ namespace AppointmentsBooking.Controllers
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,false);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Appointments");
                 }
             }
             ModelState.AddModelError("","Invalid login attempt");
--- AppointmentsBooking/Controllers/AppointmentsController.cs
+++ AppointmentsBooking/Controllers/AppointmentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppointmentsBooking.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,14 @@ namespace AppointmentsBooking.Controllers
 {
     public class AppointmentsController : Controller
     {
+        public readonly IAppointmentService _service;
+        public AppointmentsController(IAppointmentService service)
+        {
+            _service = service;
+        }
         public IActionResult Index()
         {
+            ViewBag.ListOfDoctors = _service.GetAllDoctors();
             return View();
         }
     }
