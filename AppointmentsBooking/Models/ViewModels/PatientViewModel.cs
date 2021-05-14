d36d1d9931456ee87e9a29f7d325f8b3d8aad845
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-14 14:34:12 +0200

    added repository structure

---
+++ AppointmentsBooking/Models/ViewModels/PatientViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentsBooking.Models.ViewModels
+{
+    public class PatientViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
