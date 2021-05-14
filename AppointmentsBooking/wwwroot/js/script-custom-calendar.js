b93e8df2332ebdc0883f76ab034614295e05b300
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-14 15:16:40 +0200

    render a calender

--- AppointmentsBooking/wwwroot/js/script-custom-calendar.js
+++ AppointmentsBooking/wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(document).ready(function () {
+    initilazeCalendar();
+})
+
+function initilazeCalendar() {
+    try {
+        $("#calendar").fullCalendar({
+            timezone: false,
+            header: {
+                left: 'prev,next,today',
+                center: 'title',
+                right: 'month ,agendaWeek, agendaDay'
+            },
+            selectable: true,
+            editable: false,
+
+
+        })
+    } catch (e) {
+        alert(e);
+    }
+}
