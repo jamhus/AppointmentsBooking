a769b7f1624ad64aedad059fe9d0ee902dbbcffd
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-17 01:18:18 +0200

    show events on calendar

--- AppointmentsBooking/wwwroot/js/script-custom-calendar.js
+++ AppointmentsBooking/wwwroot/js/script-custom-calendar.js
@@ -1,35 +1,69 @@
-﻿const routeUrl = location.protocol + "//" + location.host;
+﻿
+const routeUrl = location.protocol + "//" + location.host;
 $(document).ready(() => {
     $("#appointmentDate").kendoDateTimePicker({
         value: new Date(),
-        dateInput: false
+        dateInput: false,
     })
     initilazeCalendar();
 })
-
-const initilazeCalendar = () => {
-   try {
-       var calendarEl = document.getElementById('calendar');
-       if (calendarEl) {
-           var calendar = new FullCalendar.Calendar(calendarEl, {
-               initialView: 'dayGridMonth',
-               headerToolbar: {
-                   left: 'prev,next,today',
-                   center: 'title',
-                   right: 'dayGridMonth ,timeGridWeek, timeGridDay'
-               },
-               selectable: true,
-               editable: false,
-               select: (e) => {
-                   onShowModal(e, null);
-               }
-           });
-           calendar.render();
-       }
-    } catch (e) {
+const initilazeCalendar=()=> {
+    try {
+        var calendarEl = document.getElementById('calendar');
+        if (calendarEl != null) {
+           const calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {
+                    onShowModal(event, null);
+                },
+                eventDisplay: 'block',
+                events: function (fetchInfo, successCallback, failureCallback) {
+                    $.ajax({
+                        url: routeUrl + '/api/Appointment/GetCalendarData?doctorId=' + $("#doctorId").val(),
+                        type: 'GET',
+                        dataType: 'JSON',
+                        success: function (response) {
+                            var events = [];
+                            if (response.status === 1) {
+                                $.each(response.dataenum, function (i, data) {
+                                    events.push({
+                                        title: data.title,
+                                        description: data.description,
+                                        start: data.startDate,
+                                        end: data.endDate,
+                                        backgroundColor: data.isDoctorApproved ? "#28a745" : "#dc3545",
+                                        borderColor: "#162466",
+                                        textColor: "white",
+                                        id: data.id
+                                    });
+                                })
+                            }
+                            console.log(events,"events")
+                            successCallback(events);
+                        },
+                        error: function (xhr) {
+                            $.notify("Error", "error");
+                        }
+                    });
+                },
+               
+            });
+            calendar.render();
+        }
+    }
+    catch (e) {
         alert(e);
     }
+
 }
+
 
 const onShowModal = (obj,isEevntDatails) => {
     $("#appointmentInput").modal("show");
